18d6d8010059d549380b65f803d20b5353e61631
Jason Goyer <jg1000c@gmail.com>
2018-01-15 20:29:06 -0500

    yep

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from .views import PostListView, PostDetailView, PassingListView
 
@@ -8,7 +8,6 @@ urlpatterns = [
     url(r'(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
     url(r'^passing/', PassingListView.as_view(), name='passinglist'),
     url(r'^', PostListView.as_view(), name='list'),
-
 
 
 ]
